d0c46f3d86ad26253b3445de5270963a88d5e2fb
Alejo Boga <alejoboga20@gmail.com>
2022-09-28 12:11:21 -0400

    Contact form

---
+++ pages/checkout/address.tsx
@@ -0,0 +1,63 @@
+import {
+	Box,
+	Button,
+	FormControl,
+	Grid,
+	InputLabel,
+	MenuItem,
+	Select,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { ShopLayout } from '../../components/layouts';
+
+const AddressPage = () => {
+	return (
+		<ShopLayout title='Address' pageDescription='Confirm Destiny'>
+			<Typography variant='h1' component='h1'>
+				Address
+			</Typography>
+
+			<Grid container spacing={2} sx={{ mt: 2 }}>
+				<Grid item xs={12} sm={6}>
+					<TextField label='Name' variant='filled' fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField label='Lastname' variant='filled' fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField label='Address' variant='filled' fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField label='Alt Address' variant='filled' fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField label='Zip-Code' variant='filled' fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField label='City' variant='filled' fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<FormControl fullWidth>
+						<Select variant='filled' label='Country' value={1}>
+							<MenuItem value={1}>United States</MenuItem>
+							<MenuItem value={2}>Canada</MenuItem>
+							<MenuItem value={3}>Mexico</MenuItem>
+						</Select>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField label='Phone' variant='filled' fullWidth />
+				</Grid>
+			</Grid>
+
+			<Box sx={{ mt: 5 }} display='flex' justifyContent='center'>
+				<Button color='secondary' className='circular-btn' size='large'>
+					Check Order{' '}
+				</Button>
+			</Box>
+		</ShopLayout>
+	);
+};
+
+export default AddressPage;
