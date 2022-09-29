3bd2671971fd45f566d49f3fc3403453abe5147f
Alejo Boga <alejoboga20@gmail.com>
2022-09-29 11:36:00 -0400

    Get products by gender

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '../../../database';
+import { db, SHOP_CONSTANTS } from '../../../database';
 import { Product } from '../../../models';
 import { IProduct } from '../../../interfaces/products';
 
@@ -22,9 +22,16 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 }
 
 const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	let condition = {};
+	const { gender = 'all' } = req.query;
+
+	if (gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+		condition = { gender };
+	}
+
 	await db.connect();
 
-	const products = await Product.find().select('title images price inStock -_id').lean();
+	const products = await Product.find(condition).select('title images price inStock -_id').lean();
 
 	await db.disconnect();
 
