deb4d76c441ebe0174a2905c30ddc7052cfb74d4
Alejo Boga <alejoboga20@gmail.com>
2022-10-14 19:02:45 -0400

    Improve UX for search

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -26,6 +26,30 @@ export const getAllProductsSlugs = async (): Promise<ProductSlug[]> => {
 	return slugs;
 };
 
+export const getProductsByTerm = async (term: string): Promise<IProduct[]> => {
+	term = term.toString().toLowerCase();
+
+	await db.connect();
+
+	const products = await Product.find({ $text: { $search: term } })
+		.select('title images price inStock slug -_id')
+		.lean();
+
+	await db.disconnect();
+
+	return products;
+};
+
+export const getAllProducts = async (): Promise<IProduct[]> => {
+	await db.connect();
+
+	const products = await Product.find().lean();
+
+	await db.disconnect();
+
+	return JSON.parse(JSON.stringify(products));
+};
+
 interface ProductSlug {
 	slug: string;
 }
--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,26 +1,69 @@
-import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import type { NextPage, GetServerSideProps } from 'next';
+import { Box, Typography } from '@mui/material';
 import { ShopLayout } from '../../components/layouts';
 import { ProductList } from '../../components/products';
-import { useProducts } from '../../hooks';
-import { FullScreenLoading } from '../../components/ui';
+import { dbProducts } from '../../database';
+import { IProduct } from '../../interfaces/products';
 
-const SearchPage: NextPage = () => {
-	const { products, isLoading } = useProducts('/search/cybertruck');
-
+const SearchPage: NextPage<SearchPageProps> = ({ products, foundProducts, query }) => {
 	return (
 		<ShopLayout title='Testlo-Shop - Search' pageDescription='Search Products'>
 			<Typography variant='h1' component='h1'>
 				Search Products
 			</Typography>
 
-			<Typography variant='h2' component='h2' sx={{ mb: 1 }}>
-				Products
-			</Typography>
+			{foundProducts ? (
+				<Typography variant='h2' component='h2' sx={{ mb: 1 }}>
+					Term: {query}
+				</Typography>
+			) : (
+				<Box display='flex'>
+					<Typography variant='h2' component='h2' sx={{ mb: 1 }}>
+						Not Products Found
+					</Typography>
+					<Typography variant='h2' component='h2' sx={{ ml: 1 }} color='secondary'>
+						{query}
+					</Typography>
+				</Box>
+			)}
 
-			{isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+			<ProductList products={products} />
 		</ShopLayout>
 	);
 };
 
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	const { query = '' } = params as { query: string };
+
+	if (query.length === 0) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: true,
+			},
+		};
+	}
+
+	let products = await dbProducts.getProductsByTerm(query);
+	const foundProducts = products.length >= 1;
+
+	if (!foundProducts) {
+		products = await dbProducts.getProductsByTerm('shirt');
+	}
+
+	return {
+		props: {
+			products,
+			foundProducts,
+			query,
+		},
+	};
+};
+
 export default SearchPage;
+
+interface SearchPageProps {
+	products: IProduct[];
+	foundProducts: boolean;
+	query: string;
+}
