9860f94a1fab89bce548461719a8bb974ddcd861
Alejo Boga <alejoboga20@gmail.com>
2022-10-18 09:29:10 -0400

    REST Login

---
+++ pages/api/user/login.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcryptjs';
+
+import { db } from '../../../database';
+import { User } from '../../../models';
+
+type Data =
+	| {
+			message: string;
+	  }
+	| {
+			token: string;
+			user: { name: string; email: string; role: 'admin' | 'client' };
+	  };
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	switch (req.method) {
+		case 'POST':
+			return loginUser(req, res);
+
+		default:
+			res.status(400).json({ message: 'Bad Request' });
+	}
+}
+
+const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const { email = '', password = '' } = req.body;
+
+	await db.connect();
+	const user = await User.findOne({ email }).lean();
+	await db.disconnect();
+
+	if (!user) return res.status(400).json({ message: 'Email or Password Invalid' });
+
+	if (!bcrypt.compareSync(password, user.password!))
+		return res.status(400).json({ message: 'Email or Password Invalid' });
+
+	const { name, role } = user;
+
+	return res.status(200).json({
+		token: '',
+		user: { name, role, email },
+	});
+};
