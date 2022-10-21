a830650cb63efc71840ba2f9b6233152dabf0a5b
Alejo Boga <alejoboga20@gmail.com>
2022-10-21 09:04:36 -0400

    Validate and renew token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -7,3 +7,20 @@ export const signToken = (_id: string, email: string) => {
 		expiresIn: '1d',
 	});
 };
+
+export const isValidToken = (token: string): Promise<string> => {
+	if (!process.env.JWT_SECRET_SEED) throw new Error('No JWT Seed - Check environment variables');
+
+	return new Promise((resolve, reject) => {
+		try {
+			jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+				if (err) return reject('JWT invalid');
+
+				const { _id } = payload as { _id: string };
+				resolve(_id);
+			});
+		} catch (error) {
+			reject('JWT invalid');
+		}
+	});
+};
