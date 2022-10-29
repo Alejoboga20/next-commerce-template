c6f6bbd11a5da7e9a9517f331c535c6162e9bd5f
Alejo Boga <alejoboga20@gmail.com>
2022-10-29 11:33:06 -0400

    use credentials as provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,9 +10,14 @@ export const authOptions = {
 				email: { label: 'Email', type: 'email', placeholder: 'johndoe@email.com' },
 				password: { label: 'Password', type: 'password', placeholder: 'Password' },
 			},
-			async authorize(credentials) {
-				console.log(credentials);
-				return null;
+			async authorize(credentials, req) {
+				const user = { email: credentials?.email, password: credentials?.password, role: 'admin' };
+
+				if (user) {
+					return user;
+				} else {
+					return null;
+				}
 			},
 		}),
 		GithubProvider({
@@ -20,7 +25,35 @@ export const authOptions = {
 			clientSecret: process.env.GITHUB_SECRET!,
 		}),
 	],
-	callbacks: {},
+	callbacks: {
+		async jwt({ token, account, user }) {
+			if (account) {
+				token.accessToken = account.access_token;
+
+				switch (account.type) {
+					case 'oauth':
+						//TODO: Create user or verify if it exists.
+						break;
+					case 'credentials':
+						token.user = user;
+						break;
+					default:
+						break;
+				}
+			}
+
+			console.log({ token });
+
+			return token;
+		},
+		async session({ session, token, user }) {
+			session.accessToken = token.accessToken;
+			session.user = token.user as any;
+
+			console.log({ session });
+			return session;
+		},
+	},
 };
 
 export default NextAuth(authOptions);
