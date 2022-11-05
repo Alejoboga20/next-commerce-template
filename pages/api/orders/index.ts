32682e6042529f323b1135928f330d4396659ac6
Alejo Boga <alejoboga20@gmail.com>
2022-11-05 16:29:33 -0400

    Create order in DB

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,13 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import { IOrder } from '../../../interfaces';
-import { connect } from '../../../database/db';
 import { db } from '../../../database';
-import { Product } from '../../../models';
+import { Order, Product } from '../../../models';
 
-type Data = {
-	message: string;
-};
+type Data =
+	| {
+			message: string;
+	  }
+	| IOrder;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 	switch (req.method) {
@@ -28,16 +29,31 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	const productsIds = orderItems.map((product) => product._id);
 
 	await db.connect();
-	const dbProducts = await Product.find({ _id: { $in: productsIds } }).lean();
+	const dbProducts = await Product.find({ _id: { $in: productsIds } });
 
 	try {
 		const subTotal = orderItems.reduce((prev, current) => {
-			const currentPrice = dbProducts.find((prod) => prod._id === current._id)!.price;
+			const currentPrice = dbProducts.find((prod) => prod.id === current._id)?.price;
+
 			if (!currentPrice) throw new Error('Verify Cart, Product does not exist');
 
 			return currentPrice * current.quantity + prev;
 		}, 0);
-	} catch (error) {}
 
-	return res.status(201).json(dbProducts);
+		const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+		const backendTotal = subTotal * (taxRate + 1);
+
+		if (Math.round(total) !== Math.round(backendTotal)) throw new Error('Total does not match');
+
+		const userId = session.user._id;
+		const body = { ...req.body, orderItems: orderItems.map((item) => ({ ...item, id: item._id })) };
+		const newOrder = new Order({ ...body, isPaid: false, user: userId });
+		await newOrder.save();
+
+		return res.status(201).json(newOrder);
+	} catch (error: any) {
+		await db.disconnect();
+		console.log({ error });
+		res.status(400).json({ message: error.message || '' });
+	}
 };
